ecd10be9a5014df06fc806089b1ee52d1f2c3faf
Elielson André <elielsonandre123@gmail.com>
2024-10-15 19:28:25 -0300

    refact: custom error page and product not found

--- error.vue
+++ error.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="container p-4 shadow-md text-center mx-auto ">
+    <div class="max-w-2xl p-4 shadow-md text-center mx-auto mt-[10vh] ">
         <p class="text-7xl">{{ error.statusCode }}</p>
         <hr>
-        <p class="mb-4">{{ error.statusMessage }}</p>
-        <NuxtLink to="/" class="text-blue-500">Go back to home</NuxtLink>
+        <p class="my-4">{{ error.statusMessage }}</p>
+        <button @click="handleClearError" class="btn">Go back to home</button>
     </div>
 </template>
 
 <script setup>
 defineProps(['error']);
+const handleClearError = () => clearError({ redirect: '/' });
 </script>
 
 <style lang="scss" scoped></style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -8,6 +8,10 @@ const { id } = useRoute().params;
 const uri = 'https://fakestoreapi.com/products/' + id;
 const { data: product } = await useFetch(uri, { key: id }); // that key option will re-fetch the component each time its key is different.
 
+if (!product.value) {
+    throw createError({ status: 404, statusMessage: 'Product Not Found', fatal: true }); // 'fatal' will force display error page when its not identified.
+}
+
 definePageMeta({
     layout: 'products'
 });
