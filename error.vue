674f5e10750d11b7f03cd9be3677f60bab99c677
Elielson André <elielsonandre123@gmail.com>
2024-10-15 19:16:39 -0300

    custom error page

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container p-4 shadow-md text-center mx-auto ">
+        <p class="text-7xl">{{ error.statusCode }}</p>
+        <hr>
+        <p class="mb-4">{{ error.statusMessage }}</p>
+        <NuxtLink to="/" class="text-blue-500">Go back to home</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+defineProps(['error']);
+</script>
+
+<style lang="scss" scoped></style>
