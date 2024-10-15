f681be543856e44a14af76be8200d255be1e12df
Elielson André <elielsonandre123@gmail.com>
2024-10-15 11:53:15 -0300

    route params

---
+++ pages/products/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>Product id is: {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro eius autem ex deserunt ducimus temporibus
+            assumenda maiores minus cum! Libero.</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style lang="scss" scoped></style>
