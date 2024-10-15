e06aeca874f7d208aa8324bd6fe04d9e0b0c8a2c
Elielson André <elielsonandre123@gmail.com>
2024-10-15 13:59:56 -0300

    tailwindcss instalation

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,22 +5,25 @@ definePageMeta({
 </script>
 
 <template>
-    <header>
-        <NuxtLink to="/">NuxtStore</NuxtLink>
-        <ul>
-            <li>
-                <NuxtLink to="/">Home</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/about">About</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/products">Products</NuxtLink>
-            </li>
-        </ul>
+    <header class="shadow-md">
+        <nav class="container mx-auto p-4 flex justify-between">
+
+            <NuxtLink to="/" class="font-bold text-xl text-green-600">NuxtStore</NuxtLink>
+            <ul class="flex gap-4">
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/about">About</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/products" class="btn">Products</NuxtLink>
+                </li>
+            </ul>
+        </nav>
     </header>
     <!-- output the page content here -->
-    <div>
+    <div class="container mx-auto p-4 flex justify-between border-t-2">
         <slot />
     </div>
 </template>
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,29 +1,23 @@
 <template>
-    <header>
-        <NuxtLink to="/">NuxtStore</NuxtLink>
-    </header>
-    <!-- output the page content here -->
-    <div>
+
+    <div class="container mx-auto p-4 flex justify-between border-b-2">
         <slot />
     </div>
 
     <footer>
-        <ul>
-            <li>
-                <NuxtLink to="/">Home</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/about">About</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/products">Products</NuxtLink>
-            </li>
-        </ul>
+        <nav class="container mx-auto p-4 flex justify-between">
+            <ul class="flex gap-4">
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/about">About</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/products" class="btn">Products</NuxtLink>
+                </li>
+            </ul>
+            <NuxtLink to="/" class="font-bold text-xl text-green-600">NuxtStore</NuxtLink>
+        </nav>
     </footer>
 </template>
-
-<style scoped>
-.router-link-exact-active {
-    color: rgb(0, 172, 0);
-}
-</style>
