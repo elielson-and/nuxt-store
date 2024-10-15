a0c0d15e28bdc2dcadcbad903ba8c77235cc9236
Elielson André <elielsonandre123@gmail.com>
2024-10-15 11:37:19 -0300

    routes

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>Index Page</h1>
+    <p>Current route: {{ route.path }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis facere unde dolorum impedit, reiciendis tempora
+      doloremque enim optio eligendi earum fuga maiores ipsum! Dicta laborum praesentium voluptatum, soluta corporis
+      doloribus.</p>
+  </div>
+</template>
---
+++ pages/products/index.vue
@@ -0,0 +1,11 @@
+        
+<script setup>
+
+</script>
+<template>
+    <div>
+        <p>Products page</p>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
