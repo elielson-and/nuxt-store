e295953a5103702bef2be05aa131cfd2397f27f7
Elielson André <elielsonandre123@gmail.com>
2024-10-15 22:23:09 -0300

    server routes test

---
+++ pages/server-route-test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container mx-auto">
+        <h2 class="my-8">Data rendering:</h2>
+        <div class="bg-green-200">
+            <p>{{ data.message }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const { data, error } = await useFetch('/api/handle?name=Test');
+
+</script>
