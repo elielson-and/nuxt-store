e13d434c81ceb9e24d362845a7856317cdaea020
Elielson André <elielsonandre123@gmail.com>
2024-10-15 22:31:40 -0300

    server routes params

--- server/api/handle.js
+++ server/api/handle.js
@@ -1,6 +1,7 @@
 export default defineEventHandler(async (event) => {
   const { name } = getQuery(event);
   const { age } = await readBody(event);
+
   return {
     message: `Your are ${name}, and have ${age} years old.`
   };
